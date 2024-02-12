9b4304913e7b821aabc89fa61fcb7b69d1554650
2570646k@student.gla.ac.uk <2570646k@student.gla.ac.uk>
2024-02-12 16:38:37 +0000

    Actually fixed it this time :)

--- hcs_coursework/src/App.js
+++ hcs_coursework/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import EmojiPicker from 'emoji-picker-react';
 import { Picker } from 'emoji-mart';
 
@@ -14,13 +14,17 @@ function App() {
   }
   const [password, setPassword] = useState('');
 
+
   const addEmoji = (e) => {
-    setPassword((password) =>password + e.emoji);
-    console.log(password);
+    setPassword((password) => password + e.emoji);
     console.log(e.names[0]);
     //keywordCheck(password,e.names[0]) //for i in range of words corresponding to that specific emoji - change "cat" with whatever key word we are looking for
 
-  }
+  };
+
+  useEffect(() => {
+    console.log(password);
+  }, [password]);
 
   const keywordCheck = (password, keyWord) => {
     let emojiInPassword = password.includes(keyWord);
@@ -28,47 +32,48 @@ function App() {
   }
 
 
-
-
   return (
     <div className="App">
       <header className="App-header">
         <h2>Sign in using emoji password crazy fun vibes!</h2>
 
-        <div>
-        <input
-          style={{
-            width: '400px',
-            borderRadius: '30px',
-            height: '30px',
-            border: 'none',
-            padding: '10px',
-            fontSize: '20px',
-            margin: '20px'
-          }}
+        <div style={{display: 'flex', alignItems: 'flex-start', justifyContent: 'space-between'}}>
+        <div style={{marginRight: '10px', justifyContent: 'flex-start'}}>
         
-        />
+          <input
+            placeholder="Enter your username!"
+            style={{
+              width: '400px',
+              borderRadius: '30px',
+              height: '30px',
+              border: 'none',
+              padding: '10px',
+              fontSize: '20px',
+              margin: '20px'
+            }}        
+          />
 
-        <input
-          style={{
-            width: '400px',
-            borderRadius: '30px',
-            height: '30px',
-            border: 'none',
-            padding: '10px',
-            fontSize: '20px',
-            margin: '20px'
-          }}
-          value={password}
-          //onChange={(e) => addEmoji(e.target.value)}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+          <input
+          placeholder="Enter your Password!"
+            style={{
+              width: '400px',
+              borderRadius: '30px',
+              height: '30px',
+              border: 'none',
+              padding: '10px',
+              fontSize: '20px',
+              margin: '20px'
+            }}
+            value={password}
+            //onChange={(e) => addEmoji(e.target.value)}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
 
-        <div style={{display:'flex', justifyContent:'flex-start', background:'red', margin:'10px'}}>
+        <div style={{ display: 'flex', justifyContent: 'flex-start', padding: '10px' }}>
           <EmojiPicker onEmojiClick={(emoji) => addEmoji(emoji)}/>
         </div>
-
+        </div>
       </header>
     </div>
   );
